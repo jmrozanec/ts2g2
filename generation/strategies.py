28102efa418e57fa505edd8315f2aa7c0833106a
Jože Rožanec <jmrozanec@gmail.com>
2024-02-19 08:47:51 +0100

    Update time series generation strategies.

--- generation/strategies.py
+++ generation/strategies.py
@@ -2,8 +2,7 @@ import random
 
 from core.model import GraphToTimeseriesStrategy
 
-
-class RandomWalkGenerationStrategy(GraphToTimeseriesStrategy):
+class RandomNodeSequenceGenerationStrategy(GraphToTimeseriesStrategy):
     def to_sequence(self, graph, sequence_length):
         sequence = []
         nodes = [n for n in graph.nodes()]
@@ -11,6 +10,63 @@ class RandomWalkGenerationStrategy(GraphToTimeseriesStrategy):
         while len(sequence) < sequence_length:
             node = random.choice(nodes)
             sequence = sequence + [graph.nodes[node]['value']]
+
+        return sequence
+
+class RandomNodeNeighbourSequenceGenerationStrategy(GraphToTimeseriesStrategy):
+    def to_sequence(self, graph, sequence_length):
+        sequence = []
+        nodes = [n for n in graph.nodes()]
+
+        while len(sequence) < sequence_length:
+            node = random.choice(nodes)
+            neighbour = random.choice(graph.neighbors(node))
+            sequence = sequence + [graph.nodes[neighbour]['value']]
+
+        return sequence
+
+class RandomDegreeNodeSequenceGenerationStrategy(GraphToTimeseriesStrategy):
+    def to_sequence(self, graph, sequence_length):
+        sequence = []
+        nodesWeightedTuples = [(n, float(len(graph.neighbors(n)))/float(len(graph.nodes()))) for n in graph.nodes()]
+        nodes = [n[0] for n in nodesWeightedTuples]
+        nodeWeights = [n[1] for n in nodesWeightedTuples]
+
+        while len(sequence) < sequence_length:
+            node = random.choice(nodes, p=nodeWeights)
+            sequence = sequence + [graph.nodes[node]['value']]
+
+        return sequence
+
+class RandomWalkSequenceGenerationStrategy(GraphToTimeseriesStrategy):
+    def to_sequence(self, graph, sequence_length):
+        sequence = []
+        nodes = [n for n in graph.nodes()]
+        first_node = random.choice(nodes)
+        node = first_node
+        while len(sequence) < sequence_length:
+            sequence = sequence + [graph.nodes[node]['value']]
             nodes = [n for n in graph.neighbors(node)]
+            if len(nodes) == 0:
+                break
+            node = random.choice(nodes)
+
+        return sequence
+
+class RandomWalkWithRestartSequenceGenerationStrategy(GraphToTimeseriesStrategy):
+    def to_sequence(self, graph, sequence_length):
+        sequence = []
+        nodes = [n for n in graph.nodes()]
+        first_node = random.choice(nodes)
+        node = first_node
+        while len(sequence) < sequence_length:
+            sequence = sequence + [graph.nodes[node]['value']]
+            if random.random() <0.15:
+                node = first_node
+            else:
+                nodes = [n for n in graph.neighbors(node)]
+                if len(nodes) == 0:
+                    node = first_node
+                node = random.choice(nodes)
 
         return sequence
