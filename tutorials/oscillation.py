80e38d85687e636e6884862cd9eaa94d77907d46
Žan Grčar <grcar.zan1@gmail.com>
2024-07-08 13:21:11 +0200

    Binding two graphs made with sliding window method based on time co-ocurrance.

--- tutorials/oscillation.py
+++ tutorials/oscillation.py
@@ -31,8 +31,8 @@ amazon_data = pd.read_csv(os.path.join(os.getcwd(), "amazon", "AMZN.csv"))
 amazon_data["Date"] = pd.to_datetime(amazon_data["Date"])
 amazon_data.set_index("Date", inplace=True)
 
-segment_apple = apple_data[60:600]
-segment_amazon = amazon_data[5629:5689]
+segment_apple = apple_data[60:720]
+segment_amazon = amazon_data[5629:6289]
 segment_1 = apple_data[60:1200]
 
 
@@ -45,12 +45,13 @@ def plot_timeseries(sequence, title, x_legend, y_legend, color):
     plt.xlabel(x_legend)
     plt.ylabel(y_legend)
     plt.grid(True)
-    #plt.show()
+    plt.show()
 
 
 def plot_timeseries_sequence(df_column, title, x_legend, y_legend, color):
     sequence = model.Timeseries(model.TimeseriesArrayStream(df_column)).to_sequence()
     plot_timeseries(sequence, title, x_legend, y_legend, color)
+
 
 
 #drawing two time series simultaneously
@@ -80,7 +81,6 @@ def plot_two_timeseries_sequence(df_column1, df_column2, title, x_legend, y_lege
     sequence1 = model.Timeseries(model.TimeseriesArrayStream(df_column1)).to_sequence()
     sequence2 = model.Timeseries(model.TimeseriesArrayStream(df_column2)).to_sequence()
     plot_two_timeseries(sequence1, sequence2, title, x_legend, y_legend, color1, color2)
-
 
 #conversion from time series to graph
 
@@ -124,7 +124,7 @@ def combine_nodes(graph, node_1, node_2):
         graph.add_edge(node_1, neighbor)
 
     graph.remove_node(node_2)
-    return graph
+    #return graph
 
 
 
@@ -149,7 +149,7 @@ def sliding_win_graph(data, color, sliding_win_len, column_name):
         for node_2 in list(g.nodes)[i:]:
             
             if(set(list(node_1.edges)) == set(list(node_2.edges))):
-                g = combine_nodes(g, node_1, node_2)
+                combine_nodes(g, node_1, node_2)
                 combined.append(node_1)
                 
     colors = []
@@ -157,7 +157,7 @@ def sliding_win_graph(data, color, sliding_win_len, column_name):
     #If you want to have nodes that were combined be colored differently, just change one of the colors below.
     for node in list(g.nodes):
         if(node in combined):
-            colors.append(color)
+            colors.append("blue")
         else:
             colors.append(color)
 
@@ -166,7 +166,119 @@ def sliding_win_graph(data, color, sliding_win_len, column_name):
     plt.show()
 
     
-sliding_win_graph(segment_apple, "red", 10, "Close")
-sliding_win_graph(segment_1, "green", 10, "Close")
-sliding_win_graph(segment_amazon, "pink", 5, "Close")
+#sliding_win_graph(segment_apple, "red", 10, "Close")
+#sliding_win_graph(segment_1, "green", 10, "Close")
+#sliding_win_graph(segment_amazon, "pink", 5, "Close")
+plt.show()
+
+import xml.etree.ElementTree as ET
+
+#this function parses the informations from xml file and generates time series from them
+def time_series_from_xml_file():
+    file_path = os.path.join(r'C:\Users\Žan Grčar\python\ts2g2\fundimentials', r'0198N\ReportsFinStatements.xml')
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+
+    financial_statements = root.find('FinancialStatements')
+    COAMap = financial_statements.find('COAMap')
+    interim_periods = financial_statements.find('InterimPeriods')
+    annual_periods = financial_statements.find("AnnualPeriods")
+    
+    i = 0
+    for item in COAMap:
+        name = item.get('coaItem')
+
+        elements = annual_periods.findall(f".//lineItem[@coaCode = '{name}']")
+        elements2 = interim_periods.findall(f".//lineItem[@coaCode = '{name}']")
+        column = []
+        column2 = []
+
+        for element in elements:
+            column.append(float(element.text))
+        
+        for element in elements2:
+            column2.append(float(element.text))
+
+        title = item.text + " annual"
+        title2 = item.text + " interim"
+        if i%20 == 0:
+            plot_timeseries_sequence(column, title, 'Date', 'Value', "black")
+            plot_timeseries_sequence(column2, title2, 'Date', 'Value', "black")
+        i += 1
+
+
+#time_series_from_xml_file()
+
+#This functions binds two graphs made by the method of sliding window based on time co-ocurrance 
+#and then combines identical nodes(smaller graphs).
+def connected_sliding_win_graphs(data_1, data_2, color, sliding_win_len, column_name):
+
+    g = nx.Graph()
+    node_prev = None
+    nodes_1 = []
+    nodes_2 = []
+
+    for i in range(len(data_1) - sliding_win_len):
+        segment = data_1[i:i + sliding_win_len][column_name]
+        h = return_graph(segment)
+        g.add_node(h)
+        if(node_prev != None):
+            g.add_edge(node_prev, h)
+        node_prev = h
+        nodes_1.append(h)
+
+    for i in range(len(data_2) - sliding_win_len):
+        segment = data_2[i:i + sliding_win_len][column_name]
+        h = return_graph(segment)
+        g.add_node(h)
+        if(node_prev != None):
+            g.add_edge(node_prev, h)
+        node_prev = h
+        nodes_2.append(h)
+    
+    for i in range(0, len(nodes_1)):
+        g.add_edge(nodes_1[i], nodes_2[i])
+    
+    combined_1 = []
+    combined_2 = []
+
+    nodes_1_copy = nodes_1
+    nodes_2_copy = nodes_2
+
+    for i, node_1 in enumerate(list(nodes_1)):
+        for node_2 in list(nodes_1)[i:]:
+            
+            if(set(list(node_1.edges)) == set(list(node_2.edges))):
+                combine_nodes(g, node_1, node_2)
+                combined_1.append(node_1)
+                nodes_1.remove(node_2)
+    
+    for i, node_1 in enumerate(list(nodes_2)):
+        for node_2 in list(nodes_2)[i:]:
+            
+            if(set(list(node_1.edges)) == set(list(node_2.edges))):
+                combine_nodes(g, node_1, node_2)
+                combined_2.append(node_1)
+                nodes_2.remove(node_2)
+    
+    colors = []
+
+    #If you want to have nodes that were combined be colored differently, just change one of the colors below.
+    for node in list(g.nodes):
+        if(node in combined_1):
+            colors.append("blue")
+        elif(node in combined_2):
+            colors.append("red")
+        elif node in nodes_1_copy :
+            colors.append("green")
+        else:
+            colors.append("violet")
+
+    pos=nx.spring_layout(g, seed=1)
+    nx.draw(g, pos, node_size=40, node_color=colors)
+    plt.show()
+    
+connected_sliding_win_graphs(segment_amazon, segment_apple, "red", 5, "Close")
+#connected_sliding_win_graphs(segment_amazon, segment_apple, "red", 10, "Close")
+
 plt.show()
