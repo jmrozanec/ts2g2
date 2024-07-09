2c671992140819c89d788aa57cdd5ddf027a87ef
Žan Grčar <grcar.zan1@gmail.com>
2024-07-09 17:43:06 +0200

    Drawing two time series from a connected graphs constructed with sliding window mechanism.

--- tutorials/oscillation.py
+++ tutorials/oscillation.py
@@ -4,7 +4,6 @@ nb_dir = os.path.split(os.getcwd())[0]
 if nb_dir not in sys.path:
     sys.path.append(nb_dir)
 
-
 import os
 import csv
 import numpy as np
@@ -17,25 +16,20 @@ from core import model
 from sklearn.model_selection import train_test_split
 import itertools
 
-from scipy.signal import find_peaks, welch
-
+import xml.etree.ElementTree as ET
+import random
 
 #data
 apple_data = pd.read_csv(os.path.join(os.getcwd(), "apple", "APPLE.csv"))
-
 apple_data["Date"] = pd.to_datetime(apple_data["Date"])
 apple_data.set_index("Date", inplace=True)
 
 amazon_data = pd.read_csv(os.path.join(os.getcwd(), "amazon", "AMZN.csv"))
-
 amazon_data["Date"] = pd.to_datetime(amazon_data["Date"])
 amazon_data.set_index("Date", inplace=True)
 
-#segment_apple = apple_data[60:720]
 segment_apple = apple_data[60:120]
 segment_amazon = amazon_data[5629:5689]
-segment_1 = apple_data[60:1200]
-
 
 #drawing time series
 def plot_timeseries(sequence, title, x_legend, y_legend, color):
@@ -48,11 +42,9 @@ def plot_timeseries(sequence, title, x_legend, y_legend, color):
     plt.grid(True)
     #plt.show()
 
-
 def plot_timeseries_sequence(df_column, title, x_legend, y_legend, color):
     sequence = model.Timeseries(model.TimeseriesArrayStream(df_column)).to_sequence()
     plot_timeseries(sequence, title, x_legend, y_legend, color)
-
 
 
 #drawing two time series simultaneously
@@ -77,14 +69,12 @@ def plot_two_timeseries(sequence1, sequence2, title, x_legend, y_legend, color1,
     plt.tight_layout()
     plt.show()
 
-
 def plot_two_timeseries_sequence(df_column1, df_column2, title, x_legend, y_legend, color1, color2):
     sequence1 = model.Timeseries(model.TimeseriesArrayStream(df_column1)).to_sequence()
     sequence2 = model.Timeseries(model.TimeseriesArrayStream(df_column2)).to_sequence()
     plot_two_timeseries(sequence1, sequence2, title, x_legend, y_legend, color1, color2)
 
 #conversion from time series to graph
-
 
 def sequence_to_graph(column, color):
     strategy = TimeseriesToGraphStrategy(
@@ -137,8 +127,6 @@ def combine_nodes(graph, node_1, node_2):
     graph.remove_node(node_2)
     return graph
 
-
-
 #This function when called creates a graph which nodes are smaller graphs gained from segments of original data 
 #using sliding window. It's nodes (smaller graphs) that are identical are combined into one node.
 def sliding_win_graph(data, color, sliding_win_len, column_name):
@@ -188,12 +176,9 @@ def sliding_win_graph(data, color, sliding_win_len, column_name):
     plt.show()
 
 
-
-import xml.etree.ElementTree as ET
-
 #this function parses the informations from xml file and generates time series from them
-def time_series_from_xml_file():
-    file_path = os.path.join(r'C:\Users\Žan Grčar\python\ts2g2\fundimentials', r'0198N\ReportsFinStatements.xml')
+def time_series_from_xml_file(file_path):
+
     tree = ET.parse(file_path)
     root = tree.getroot()
 
@@ -224,10 +209,7 @@ def time_series_from_xml_file():
             plot_timeseries_sequence(column2, title2, 'Date', 'Value', "black")
         i += 1
 
-
-#time_series_from_xml_file()
 #These are smaller functions to support ones below:
-import random
 
 #finds node with lowest index
 def min_node(neighbors, nodes):
@@ -281,7 +263,7 @@ def append_random(sequence, graph):
 
 #This function draws a graph made with sliding window mechanism by iterating through the first graph, 
 #always choosing lowest available index of a node and value. It simultaniously deletes paths it has already crossed.
-def graphToTime_sequence_one_least(graph, nodes_1, nodes_2, color_1, color_2, time_series_len = 100):
+def toTimeSequence_lowestIndex(graph, nodes_1, nodes_2, color_1, color_2, time_series_len = 100):
     sequence_1 = []
     sequence_2 =[]
 
@@ -351,7 +333,7 @@ def graphToTime_sequence_one_least(graph, nodes_1, nodes_2, color_1, color_2, ti
 
 #This function draws a graph made with sliding window mechanism by randomly choosing next node and value always from the same graph, 
 # and for the other it chooses a random neighbor of the current node from the first graph
-def graphToTime_sequence_one_random(graph, nodes_1, nodes_2, color_1, color_2, time_series_len = 100):
+def toTimeSequence_lowestIndex_random(graph, nodes_1, nodes_2, color_1, color_2, time_series_len = 100):
     sequence_1 = []
     sequence_2 =[]
 
@@ -376,7 +358,7 @@ def graphToTime_sequence_one_random(graph, nodes_1, nodes_2, color_1, color_2, t
     plot_timeseries(sequence_2, "graph 2", "Date", "Value", color_2)
 
 #This function draws a graph made with sliding window mechanism by randomly choosing next node and value once from fist graph and once from the other
-def graphToTime_sequence_random(graph, nodes_1, nodes_2, color_1, color_2, time_series_len = 100):
+def toTimeSequence_random(graph, nodes_1, nodes_2, color_1, color_2, time_series_len = 100):
     sequence_1 = []
     sequence_2 =[]
 
@@ -403,9 +385,6 @@ def graphToTime_sequence_random(graph, nodes_1, nodes_2, color_1, color_2, time_
     
     plot_timeseries(sequence_1, "graph 1", "Date", "Value", color_1)
     plot_timeseries(sequence_2, "graph 2", "Date", "Value", color_2)
-
-            
-
 
 #This functions binds two graphs made by the method of sliding window based on time co-ocurrance 
 #and then combines identical nodes(smaller graphs).
@@ -503,7 +482,6 @@ def connected_sliding_win_graphs(data_1, data_2, sliding_win_len, column_name):
     #graphToTime_sequence_one_least(g, nodes_1, nodes_2, "red", "blue", time_series_len=200)
     #graphToTime_sequence_one_random(g, nodes_1, nodes_2, "green", "black", time_series_len=200)
     #graphToTime_sequence_random(g, nodes_1, nodes_2, "grey", "purple", time_series_len=200)
-
 
 connected_sliding_win_graphs(segment_amazon, segment_apple, 5, "Close")
 
